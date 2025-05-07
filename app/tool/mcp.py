36627a12e2dc58238d9a40a6820c2662146667a5
liangxinbing <1580466765@qq.com>
2025-05-07 11:55:10 +0800

    format code

--- app/tool/mcp.py
+++ app/tool/mcp.py
@@ -1,14 +1,14 @@
 from contextlib import AsyncExitStack
 from typing import Dict, List, Optional
 
-from mcp import ClientSession, StdioServerParameters
 from mcp.client.sse import sse_client
 from mcp.client.stdio import stdio_client
-from mcp.types import TextContent, ListToolsResult
+from mcp.types import ListToolsResult, TextContent
 
 from app.logger import logger
 from app.tool.base import BaseTool, ToolResult
 from app.tool.tool_collection import ToolCollection
+from mcp import ClientSession, StdioServerParameters
 
 
 class MCPClientTool(BaseTool):
